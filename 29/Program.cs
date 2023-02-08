5ce049b6550479612e81d3ae83558e2148a91843
karina <karyna.rein@gmail.com>
2023-02-08 22:56:20 +0300

    решена задача в папке 29

---
+++ 29/Program.cs
@@ -0,0 +1,22 @@
+﻿int [] FillArray()
+{
+    int [] Array = new int [8];
+    Random number = new Random();
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = number.Next(0,100);
+    }
+    return Array;
+}
+
+int [] arr = FillArray();
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+}
+
+PrintArray(arr);
