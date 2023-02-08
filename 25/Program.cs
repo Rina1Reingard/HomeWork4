08593ce1a1d309ebd58b2724f42a53e92a354ac3
karina <karyna.rein@gmail.com>
2023-02-08 22:52:54 +0300

    решена задача в папке 25

---
+++ 25/Program.cs
@@ -0,0 +1,26 @@
+﻿int Number(string message)
+{
+    int number = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out number))
+        {break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или некорректное число. Повторите ввод");
+        }
+    }
+    return number;
+}
+
+int a = Number ("Введите первое число");
+int b = Number ("Введите второе число");
+int result = a;
+
+for (int i = 1; i < b; i++)
+{
+    result = result*a;   
+}
+Console.Write($"{result}");
